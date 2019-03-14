0c595a962e7607fd6a4745a2a1f32db569581d5b
Catalin Mihai <catalinnm99@gmail.com>
2019-03-14 21:49:21 +0200

    Added a switch menu

--- main.cpp
+++ main.cpp
@@ -11,47 +11,68 @@ using namespace std;
 
 int main()
 {
-    /*ifstream f("input_DFA2.txt");
-    DFA dfa;
-    f>>dfa;
-    int nr_cuv;
-    f>>nr_cuv;
-    dfa.Afiseaza();
-    string cuvant;
-    for(int i = 0; i < nr_cuv; i++)
+    cout<<"Introduceti numarul testului: "<<endl;
+    cout<<"1 -> DFA"<<endl;
+    cout<<"2 -> NFA"<<endl;
+    cout<<"3 -> lNFA"<<endl;
+    int op;
+    cin>>op;
+    switch(op)
     {
-        f>>cuvant;
-        bool t = dfa.CheckWord(cuvant);
-        if(t == false) cout<<"Nu este bun cuvantul: "<<cuvant<<endl;
-        else cout<<"Este bun cuvantul: "<<cuvant<<endl;
-    }*/
-    /*ifstream f("input_NFA.txt");
-    NFA nfa;
-    f>>nfa;
-    int nr_cuv;
-    f>>nr_cuv;
-    nfa.Afiseaza();
-    string cuvant;
-    for(int i = 0; i < nr_cuv; i++)
-    {
-        f>>cuvant;
-        bool t = nfa.CheckWord(cuvant);
-        if(t == false) cout<<"Nu este bun cuvantul: "<<cuvant<<endl;
-        else cout<<"Este bun cuvantul: "<<cuvant<<endl;
-    }*/
-    ifstream f("input_LNFA.txt");
-    LNFA lnfa;
-    f>>lnfa;
-    int nr_cuv;
-    f>>nr_cuv;
-    lnfa.Afiseaza();
-    string cuvant;
-    for(int i = 0; i < nr_cuv; i++)
-    {
-        f>>cuvant;
-        bool t = lnfa.CheckWord(cuvant);
-        if(t == false) cout<<"Nu este bun cuvantul: "<<cuvant<<endl;
-        else cout<<"Este bun cuvantul: "<<cuvant<<endl;
+    case 1:
+        {
+            ifstream f("input_DFA2.txt");
+            DFA dfa;
+            f>>dfa;
+            int nr_cuv;
+            f>>nr_cuv;
+            dfa.Afiseaza();
+            string cuvant;
+            for(int i = 0; i < nr_cuv; i++)
+            {
+                f>>cuvant;
+                bool t = dfa.CheckWord(cuvant);
+                if(t == false) cout<<"Nu este bun cuvantul: "<<cuvant<<endl;
+                else cout<<"Este bun cuvantul: "<<cuvant<<endl;
+            }
+            break;
+        }
+    case 2:
+        {
+            ifstream f("input_NFA.txt");
+            NFA nfa;
+            f>>nfa;
+            int nr_cuv;
+            f>>nr_cuv;
+            nfa.Afiseaza();
+            string cuvant;
+            for(int i = 0; i < nr_cuv; i++)
+            {
+                f>>cuvant;
+                bool t = nfa.CheckWord(cuvant);
+                if(t == false) cout<<"Nu este bun cuvantul: "<<cuvant<<endl;
+                else cout<<"Este bun cuvantul: "<<cuvant<<endl;
+            }
+            break;
+        }
+    case 3:
+        {
+            ifstream f("input_LNFA.txt");
+            LNFA lnfa;
+            f>>lnfa;
+            int nr_cuv;
+            f>>nr_cuv;
+            lnfa.Afiseaza();
+            string cuvant;
+            for(int i = 0; i < nr_cuv; i++)
+            {
+                f>>cuvant;
+                bool t = lnfa.CheckWord(cuvant);
+                if(t == false) cout<<"Nu este bun cuvantul: "<<cuvant<<endl;
+                else cout<<"Este bun cuvantul: "<<cuvant<<endl;
+            }
+            break;
+        }
     }
     return 0;
 }
